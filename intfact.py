588b1cfdee276130fb3ef33151ee190bf9674e15
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-10 15:31:35 +0530

    code

--- intfact.py
+++ intfact.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 import sys
-from mpmath import *
+from _zeros import zeros
 from pi import pi
 from e import e
-from zeros import zeros
+
+def read(f, zz):
+    pp = ""
+    ctr = 0
+    f.seek(0)
+    while ctr < zz:
+        c = str(f.read(1))
+        ctr = ctr + 1
+        pp = pp + c
+    return pp
 
 def characterize(num, l, pp, s, prec):
+    fp = open("./pi.txt","r")
+    fe = open("./e.txt","r")
     pp_index = 0
     state_vector_pp = []
+    state_vector = []
     net_maturity = 0
     hit2 = 0
     while hit2 < l:
@@ -15,13 +27,13 @@ def characterize(num, l, pp, s, prec):
         base_ctr = 0
         while True:
             ctr = 0
-            pz = pi[:(base_ctr + ctr + 1)]
-            ez = e[:(base_ctr + ctr + 1)][::-1]
+            pz = read(fp, (base_ctr + ctr + 1))
+            ez = read(fe, (base_ctr + ctr + 1))[::-1]
             maturity = 0
             for x in list(zip(pz, ez)):
                 if x[0] == x[1]:
                     maturity = maturity + 1
-            maturity = maturity % 100
+            maturity = maturity % 10
             index = pp.index(str(maturity)[::-1], pp_index)
             if index == pp_index:
                 net_maturity = net_maturity + maturity
@@ -30,7 +42,7 @@ def characterize(num, l, pp, s, prec):
                 ctr = ctr + 1
                 base_ctr = 0
                 state_vector_pp.append(pz)
-                if hit % l == 0:
+                if hit == l:
                     break
             else:
                 base_ctr = base_ctr + l
@@ -45,21 +57,17 @@ def characterize(num, l, pp, s, prec):
                 if not net_maturity in zeros[init_index:]:
                     break
             zetazeros = []
-            mp.prec = prec
-            mp.dps = prec
-            """
-            for x in zidx:
-                zetazeros.append(str(zetazero(zidx[0]+1).imag))
-            print([zidx, net_maturity, zetazeros])
-                """
-            print([zidx, net_maturity])
-            if hit2 % l == 0:
-                sys.exit(2)
+#            print([hit2,zidx, net_maturity])
+            state_vector.append(net_maturity)
+            if hit2 == l:
+                fp.close()
+                fe.close()
+                return state_vector
 
 def factorize(num, pp, s, prec):
     l = len(num)
-    factor = characterize(num, l, pp, s, prec)
-    return factor
+    state_vector = characterize(num, l, pp, s, prec)
+    return state_vector
 
 num=str(sys.argv[1])
 print("Number Entered was: " + str(num))
@@ -67,7 +75,5 @@ c = num.count("0")
 s = sum(map(int, num)) + 10*c
 prec = 2*s
 print("Sum of digits : " + str(s))
-state_vector1 = factorize(num, pi, s, prec)
-state_vector2 = factorize(num, e, s, prec)
-print(state_vector1)
-print(state_vector2)
+state_vector = factorize(num, pi, s, prec)
+print(state_vector)
