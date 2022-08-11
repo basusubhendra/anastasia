cccda13ba0dada7aee2d37f8557c7a2a28987d7d
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-11 17:54:32 +0530

    code

--- intfact.py
+++ intfact.py
@@ -4,6 +4,9 @@ from characterize import *
 from pi import pi
 from e import e
 
+def factorize(sv, pp, ee):
+    pass
+
 if __name__ == "__main__":
     num = str(sys.argv[1])
     print("Number Entered was : " + str(num))
@@ -12,3 +15,7 @@ if __name__ == "__main__":
     state_vector2 = characterize(l, e)
     print(state_vector1)
     print(state_vector2)
+    _state_vector_1_pi = factorize(state_vector1, pi, e)
+    _state_vector_1_e = factorize(state_vector1, e, pi)
+    _state_vector_2_pi = factorize(state_vector2, pi, e)
+    _state_vector_2_e = factorize(state_vector2, e, pi)
