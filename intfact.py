f92c28df01bf6fb01bd788711672be6beca1e319
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-12 08:48:04 +0530

    code

--- intfact.py
+++ intfact.py
@@ -9,14 +9,29 @@ def factorize(num, sv, pp, ee):
     ctr = 0
     s1 = sv[ctr]
     stats = []
+    l = len(num)
+    mid = int(l / 2)
     while ctr < (len(sv) - 1):
        s2 = sv[ctr + 1]
        if s2 >= s1:
            t = 1
        elif s2 < s1:
            t = -1
-       #calculate condition from num
-       #depending on whether s1 ><= s2
+       nn = ""
+       if s1 < s2:
+           n1 = num[-(ctr + 1)]
+           n2 = num[-(ctr + 2)]
+           if ctr <= mid:
+               nn = n2 + n1
+           else:
+               nn = n1 + n2
+       elif s1 > s2:
+           n1 = num[ctr]
+           n2 = num[ctr + 1]
+           if ctr <= mid:
+               nn = n1 + n2
+           else:
+               nn = n2 + n1
        for x in range(s1, s2+t, t):
            _pp = pp[:x]
            _ee = ee[:x][::-1]
@@ -33,11 +48,13 @@ def factorize(num, sv, pp, ee):
                else:
                    zs2 = int(z[1])
                sum_x = sum_x + zs1 + zs2
-               #condition here TBD
-       #if condition
-       stats.append(ctr)
+               if sum_x >= int(nn):
+                   break
+       if sum_x == int(nn):
+           stats.append(ctr)
        ctr = ctr + 1
        s1 = sv[ctr]
+    print(stats)
     return stats
 
 if __name__ == "__main__":
@@ -70,7 +87,13 @@ if __name__ == "__main__":
     factorize(num, state_vector1, pi, e)
     #_state_vector_1_pi = factorize(num, state_vector1, pi, e)
     #print(_state_vector_1_pi)
-    print("End of Pi-E Factorization Phase")
+    end = time.process_time()
+    print("Time Taken = " + str(end-start))
+    print(" ")
+    start = time.process_time()
+    factorize(num, state_vector2, pi, e)
+    #_state_vector_1_pi = factorize(num, state_vector1, pi, e)
+    #print(_state_vector_1_pi)
     end = time.process_time()
     print("Time Taken = " + str(end-start))
     print(" ")
