f486bc78092cb8a271d168c02815fb1c5ab7ebba
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-15 10:37:29 +0530

    code

--- intfact.py
+++ intfact.py
@@ -3,12 +3,16 @@ import sys
 from zeros1 import zeros1
 from characterize import *
 from solver import *
+from primes import *
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     ctr = 1
     s0 = ""
     s1 = ""
+    nzeros = 0
+    haves = 0
+    have_nots = 0
     while True:
         print("==== " + str(ctr-1) + "=====")
         zz = characterize(int(ctr), pi)
@@ -31,8 +35,13 @@ if __name__ == "__main__":
             zz_set = zz_set[::-1]
         snippet = int(solver(zz_set, num))
         print(snippet % 7)
+        if snippet % 7 == 0:
+            nzeros = nzeros + 1
         if (ctr-1) % 7 == 0:
             print(" ")
-            input(" ")
+            haves = haves + nzeros
+            have_nots = have_nots + (7 - nzeros)
+            input(["nzeros =  " + str(nzeros), haves, have_nots, "zeros" , [haves in zeros], [have_nots in zeros], "primes" , [haves in primes, have_nots in primes]])
+            nzeros = 0
         ctr = ctr + 1 
         s0 = int(s1)
