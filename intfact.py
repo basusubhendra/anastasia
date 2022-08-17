77d043dd21b1e69f723bb40218e6252142b3cf57
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-17 08:10:01 +0530

    Characterization Code ready

--- intfact.py
+++ intfact.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 import sys
 
+RUN_LENGTH=3
+def characterize(num):
+    l = len(num)
+    pos = 0
+    offsets = []
+    while True:
+        _nn = ""
+        ctr = pos
+        while ctr < pos + RUN_LENGTH:
+            _nn = _nn + num[ctr % l]
+            ctr = ctr + 1
+        input(_nn)
+        offsets.append(_nn)
+        if ctr % l == 0:
+            break
+        pos = pos + RUN_LENGTH + l
+    return offsets
+
 if __name__ == "__main__":
     num = str(sys.argv[1])
     print("Number Entered was: " + str(num))
+    triplets = characterize(num)
+    print(triplets)
 
