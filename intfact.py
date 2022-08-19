2fa746eb30edbacd9a4919a8e491d6a601206c3d
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-19 22:00:35 +0530

    Code

--- intfact.py
+++ intfact.py
@@ -5,11 +5,7 @@ from e import e
 from zeros import zeros
 from primes import primes
 
-pi10=['3','0','1','4','1','5','9','2','6','5']
-e10=['2','0','7','1','8','2','8','1','8','2']
 def factorize(num):
-    global pi10
-    global e10
     l = len(num)
 
 if __name__ == "__main__":
