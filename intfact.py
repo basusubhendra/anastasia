a59ce022fac696247295583abfe61de2ea3529d9
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-19 12:21:14 +0530

    code

--- intfact.py
+++ intfact.py
@@ -23,14 +23,18 @@ def factorize(num):
     t = [0, 1]
     f = open("./pi.txt","r")
     g = open("./e.txt","r")
-    reconstructed_number = ""
-    pi_vector = []
-    e_vector = []
-    pi_ones = 0
-    e_ones = 0
+    acc1 = []
+    acc2 = []
+    _acc1 = [nk1]
+    _acc2 = [nk2]
+    prev_pos1 = -1
+    prev_pos2 = -1
+    pos1 = 0
+    pos2 = 0
     while True:
         c = str(f.read(1))
         d = str(g.read(1))
+        input([c, d])
         parity1 = 0
         parity2 = 0
         finished_nk1 = ""
@@ -39,7 +43,6 @@ def factorize(num):
                cnt1 = cnt1 + 1
                nk1 = nk1 - 1
                if nk1 == 0:
-                   finished_nk1 = num[ctr1 % l]
                    cnt1 = 0
                    t[0] = 1 - t[0]
                    ctr1 = ctr1 + 1
@@ -47,11 +50,30 @@ def factorize(num):
                    if nk1 == 0:
                        nk1 = 10
                    parity1 = 1
+                   c = str(f.read(1))
+                   _pos = f.tell()
+                   if (t[0] == 0 and c == e10[cnt1]) or (t[0] == 1 and c == pi10[cnt1]):
+                       _acc1.append(num[ctr1 % l]) 
+                       parity1 = 0
+                       cnt1 = cnt1 + 1
+                       nk1 = nk1 - 1
+                       if nk1 == 0:
+                            cnt1 = 0
+                            t[0] = 1 - t[0]
+                            ctr1 = ctr1 + 1
+                            nk1 = int(num[ctr1 % l])
+                            if nk1 == 0:
+                                nk1 = 10
+                            parity1 = 1
+                   else:
+                       _acc1 = [ nk1]
+                       f.seek(_pos-1, 0)
+                   acc1.append(_acc1)
+                   _acc1 = []
         if (t[1] == 1 and d == pi10[cnt2]) or (t[1] == 0 and d == e10[cnt2]):
                cnt2 = cnt2 + 1
                nk2 = nk2 - 1
                if nk2 == 0:
-                   finished_nk2 = num[ctr2 % l]
                    cnt2 = 0
                    t[1] = 1 - t[1]
                    ctr2 = ctr2 + 1
@@ -59,18 +81,36 @@ def factorize(num):
                    if nk2 == 0:
                        nk2 = 10
                    parity2 = 1
+                   d = str(g.read(1))
+                   _pos = g.tell()
+                   if (t[1] == 1 and d == pi10[cnt2]) or (t[1] == 0 and d == e10[cnt2]):
+                       _acc2.append(num[ctr2 % l]) 
+                       parity2 = 0
+                       cnt2 = cnt2 + 1
+                       nk2 = nk2 - 1
+                       if nk2 == 0:
+                            cnt2 = 0
+                            t[1] = 1 - t[1]
+                            ctr2 = ctr2 + 1
+                            nk2 = int(num[ctr2 % l])
+                            if nk2 == 0:
+                                nk2 = 10
+                            parity2 = 1
+                   else:
+                       _acc2 = [ nk2]
+                       g.seek(_pos-1, 0)
+                   acc2.append(_acc2)
+                   _acc2 = []
         if parity1 == 1 and parity2 == 1:
-            pi_vector.append(str(bin(pi_ones)[2:])[::-1])
-            e_vector.append(str(bin(e_ones)[2:])[::-1])
-            pi_ones = 0
-            e_ones = 0
-            input("Enter")
+            input("pi-e hit")
         elif parity1 == 1 and parity2 == 0:
-            pi_ones = pi_ones + 1
+            input("pi_hit")
         elif parity1 == 0 and parity2 == 1:
-            e_ones = e_ones + 1
+            input("e_hit")
         elif parity1 == 0 and parity2 == 0:
             pass
+        pos1 = pos1 + 1
+        pos2 = pos2 + 1
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
