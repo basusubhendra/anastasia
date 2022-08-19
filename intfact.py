31dea04d59a9ffa3d592c112c7bf1227d7460a69
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-19 10:43:38 +0530

    Faster Runtime

--- intfact.py
+++ intfact.py
@@ -6,58 +6,6 @@ from e import e
 pi10=['3','0','1','4','1','5','9','2','6','5']
 e10=['2','0','7','1','8','2','8','1','8','2']
 
-def _analyze(num11, sums):
-    ctr = 0
-    s0 = sums[ctr]
-    ctr = ctr + 1
-    success = False
-    cnt = 0
-    l = len(num)
-    pivots = []
-    while ctr < len(sums):
-        s = sums[ctr]
-        delta = s0 - s
-        if delta > 0 and delta == int(num11[cnt]):
-            s0 = s
-            cnt = cnt + 1
-            pivots.append(ctr)
-            if cnt == l:
-                return True, pivots
-        ctr = ctr + 1
-    return False, None
-
-def analyze(num, pi_hits, e_hits, pp, ee):
-    sums = []
-    for x in list(zip(pp, pi_hits, e_hits, ee)):
-        x0 = 0
-        x1 = 0
-        x2 = 0
-        x3 = 0
-        if int(x[0]) == 0:
-            x0 = 10
-        else:
-            x0 = int(x[0])
-        if int(x[1]) == 0:
-            x1 = 10
-        else:
-            x1 = int(x[1])
-        if int(x[2]) == 0:
-            x2 = 10
-        else:
-            x2 = int(x[2])
-        if int(x[3]) == 0:
-            x3 = 10
-        else:
-            x3 = int(x[3])
-        sums.append(set(sorted([int(x[0]), int(x[1]), int(x[2]), int(x[3])])))
-    success =  sums[0] in sums[1:]
-    index = -1
-    if success == True:
-        index = sums[1:].index(sums[0])
-    else:
-        index = -1
-    return success, index
-
 def factorize(num):
     global pi10
     global e10
@@ -79,8 +27,11 @@ def factorize(num):
     t3 = 0
     reconstructed_number = ""
     hit = 0
+    mid = int(l / 2)
     pi_hits = []
     e_hits = []
+    pi_ones = 0
+    e_ones = 0
     while True:
         c = str(f.read(1))
         d = str(g.read(1))
@@ -113,19 +64,18 @@ def factorize(num):
                        nk2 = 10
                    parity2 = 1
         if parity1 == 1 and parity2 == 1:
-            pi_hits.append(num[ctr1 % l])
-            e_hits.append(num[ctr2 % l])
+            print([pi_ones, e_ones])
+            pi_ones = 0
+            e_ones = 0
+            if hit == mid:
+                sys.exit(0)
             hit = hit + 1
-            pp = pi[:hit]
-            ee = e[:hit][::-1]
-            success, delta = analyze(num, pi_hits, e_hits, pp, ee)
-            if success == True:
-                input([success, delta])
-            """
-            #success, bin_factor1, bin_factor2 = analyze(pi_hits, e_hits, pp, ee)
-            if success == True:
-                return int(bin_factor1), int(bin_factor2)
-            """
+        elif parity1 == 1 and parity2 == 0:
+            pi_ones = pi_ones + 1
+        elif parity1 == 0 and parity2 == 1:
+            e_ones = e_ones + 1
+        elif parity1 == 0 and parity2 == 0:
+            pass
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
