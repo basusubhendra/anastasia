54f39540233fbfa24a113b779a552f1a229fb7f2
Subhendra Basu <basu.subhendra@outlook.com>
2022-08-20 08:27:26 +0530

    Code

--- intfact.py
+++ intfact.py
@@ -4,14 +4,43 @@ import sys
 def factorize(num):
     l = len(num)
     f = open("./pi.txt","r")
-    g = open("./e.txt", "r")
+    #slowest running counter
     ctr = 0
-    _nk1 = num[ctr % l]
-    _nk2 = num[(ctr + 1) % l]
+    #fast running counter
+    count = 0
+    _pivots = []
+    while ctr < l - 1:
+        _nk1 = num[ctr % l]
+        _nk2 = num[(ctr + 1) % l]
+        cnt = 0
+        nk = num[cnt % l]
+        hit = 0
+        #pi digit specific counter
+        pivots = []
+        target = _nk1
+        f.seek(0)
+        while True:
+            c = str(f.read(1))
+            __nk = num[count % l]
+            if c == nk:
+                cnt = cnt + 1
+                nk = num[cnt % l]
+                if cnt % l == 0:
+                    hit = hit + 1
+                if __nk == target and cnt % l == 0:
+                    pivots.append(hit)
+                    if target == _nk2:
+                        count = count + 1
+                        break
+                    target = _nk2
+            count = count + 1
+        _pivots.append(pivots)
+        ctr = ctr + 1
     f.close()
-    g.close()
+    return _pivots
 
 if __name__ == "__main__":
     num = str(sys.argv[1])
     print("Number Entered was : " + str(num))
-    factorize(num)
+    pivots = factorize(num)
+    print(pivots)
